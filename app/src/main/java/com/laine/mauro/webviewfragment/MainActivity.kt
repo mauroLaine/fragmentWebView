5c0c1655f41fd7822ffc153adfe831b6018e2b05
Mauricio Laine <laine.mau@gmail.com>
2019-07-29 21:31:42 -0700

    load google.com inside the webview fragment

--- app/src/main/java/com/laine/mauro/webviewfragment/MainActivity.kt
+++ app/src/main/java/com/laine/mauro/webviewfragment/MainActivity.kt
@@ -2,15 +2,19 @@ package com.laine.mauro.webviewfragment
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_load.setOnClickListener {
+            supportFragmentManager.beginTransaction().add(R.id.fragment_container, WebFragment()).commit()
+        }
     }
 
-    companion object{
+    companion object {
         val WEB_VIEW_URL = "http://www.google.com"
     }
 }
